b7f7c9fd17e18ed75beeed6f5b6c0ff67095fecc
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2023-06-26 01:36:40 -0700

    Initial Commit

---
+++ RISCV-JVM/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RISCV-JVM
+//
+//  Created by HE Siyao on 26/6/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
