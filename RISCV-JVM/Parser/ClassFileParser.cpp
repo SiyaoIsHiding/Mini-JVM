63863f96372a4b525b6ea6a7dd69915b37880f62
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2023-06-30 00:16:59 -0700

    Versions

--- RISCV-JVM/Parser/ClassFileParser.cpp
+++ RISCV-JVM/Parser/ClassFileParser.cpp
@@ -7,15 +7,26 @@
 
 #include "ClassFileParser.hpp"
 
+InstanceKlass* ClassFileParser::Parser(ClassReader* classReader)
+{
+    InstanceKlass* klass = new InstanceKlass;
+    putMagic(classReader, klass);
+    putMinorVersion(classReader, klass);
+    putMajorVersion(classReader, klass);
+    return klass;
+}
+
 void ClassFileParser::putMagic(ClassReader* classReader, InstanceKlass* klass)
 {
     int magic = classReader->read4Byte();
     klass->setMagic(magic);
 }
 
-InstanceKlass* ClassFileParser::Parser(ClassReader* classReader)
-{
-    InstanceKlass* klass = new InstanceKlass;
-    putMagic(classReader, klass);
-    return klass;
+void ClassFileParser::putMajorVersion(ClassReader *classReader, InstanceKlass *klass) { 
+    klass->setMajorVersion(classReader->read2Byte()) ;
 }
+
+void ClassFileParser::putMinorVersion(ClassReader *classReader, InstanceKlass *klass) { 
+    klass->setMinorVersion(classReader->read2Byte());
+}
+
